d869c41bad9ae610b949f287fa89316084e2d20a
CateHey <catyvaras19@gmail.com>
2021-12-03 16:32:10 -0500

    program

---
+++ app.py
@@ -0,0 +1,34 @@
+import requests
+import json
+from dataframe import getDataframe
+
+headers = {"x-hasura-admin-secret"  : "x5cHTWnDb7N2vh3eJZYzamgsUXBVkw",
+           "content-type":"application/json"}
+
+def run_query(query,variables):
+    request = requests.post('https://graph.sop.strategio.cloud/v1beta1/relay', json={'query': query ,"variables":variables}, headers=headers,)
+    if request.status_code == 200:
+        print("Ingresado Correctamente")
+        return request.json()
+        
+    else :
+        raise print(Exception('Error Mutacion'))
+    
+query = """
+  mutation Sellin($objects: [Sell_in_insert_input!]!) {
+  insert_Sell_in(objects: $objects, on_conflict: {constraint:Sell_in_pkey , update_columns:nart }) {
+    returning {
+      nart
+    }
+  }
+}
+
+"""
+getDataframe()
+
+d = open('data/variables.json')
+variables = json.load(d)
+
+#print(variables)
+
+run_query(query,variables)
